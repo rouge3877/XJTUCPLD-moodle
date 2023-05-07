8f2fd6a32051a6af0a4d5bc5bd73c3ef03fdbabb
ROUGe <rouge_3877@outlook.com>
2023-05-07 22:08:38 +0800

    Complete 8.16

--- Ch8/8.16_numINstr.c
+++ Ch8/8.16_numINstr.c
@@ -1,87 +1,41 @@
 ﻿#include <stdio.h>
 #include <string.h>
 
-//#include <stdlib.h>
-
-int count(const char *);
-void Findnum(const char *,int,int);
-
-int main(void)
+int main()
 {
     char str[100];
+    int a[50];
+    int len = 0, num = 0, i, j;
+
     gets(str);
 
-    int num[100];
+    for (i = 0; i < strlen(str); i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {                                  // 如果是数字字符
+            num = num * 10 + str[i] - '0'; // 将连续的数字字符组成一个整数
+        }
+        else
+        {
+            if (num > 0)
+            { // 如果前面有数字，将其存入数组a中
+                a[len++] = num;
+                num = 0;
+            }
+        }
+    }
 
-    int numtotal = count(str);
-    printf("%d ", numtotal);
-    Findnum(str, strlen(str), numtotal);
+    if (num > 0)
+    { // 如果最后一个字符是数字，将其存入数组a中
+        a[len++] = num;
+    }
 
-    //system("pause");
+    printf("%d ", len); // 输出整数的个数
+
+    for (j = 0; j < len; j++)
+    { // 输出数组a中的每个元素
+        printf("%d ", a[j]);
+    }
+
     return 0;
-}
-
-int count(const char *str)
-{
-    int num = 0;
-    int i = 0;
-    while (str[i+1] != '\0')
-    {
-        if ((str[i] >= '0' && str[i] <= '9') && ((str[i + 1] < '0' || str[i + 1] > '9') || (str[i + 1] == '\0')))
-        {
-            num++;
-        }
-        i++;
-    }
-
-    return num;
-}
-
-void Findnum(const char *str, int len, int num)
-{
-    static numlist[100] = {0};
-
-    int i;
-    int left[100]={0};
-    int right[100]={0};
-    int j=0;
-
-    for(i=0;i<len;i++)
-    {
-        
-        if ((str[i] >= '0' && str[i] <= '9') && ((str[i-1] < '0' || str[i-1] > '9')||i==0)){
-            left[j] = i;
-            j++;
-        }
-
-        
-    }
-
-    int k=0;
-
-    for(i=0;i<len-1;i++)
-    {
-        
-        if ((str[i] >= '0' && str[i] <= '9') && ((str[i + 1] < '0' || str[i + 1] > '9') || (str[i + 1] == '\0')))
-        {
-           right[k] = i;
-           k++;
-        }
-    }
-
-    for(i=0;i<num;i++)
-    {
-        int num;
-        for(j=left[i];j<=right[i];j++)
-        {
-            num = num + (str[j]-'0')*10^(right[i]-j);
-        }
-        numlist[i] = num;
-
-    }
-
-    for(i=0;i<num;i++)
-    {
-        printf("%d ",numlist[i]);
-    }
 }
