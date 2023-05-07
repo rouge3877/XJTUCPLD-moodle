e34b96fdd71c1b6d8cda3824a581ff6e8575c5d7
ROUGe <rouge_3877@outlook.com>
2023-05-08 00:10:13 +0800

    finish 9.1-9.2

---
+++ Ch9/9.4_input9.3.c
@@ -0,0 +1,10 @@
+/*在上题(9.3)基础上，编写一个input函数，用来输入5个学生的成绩。
+以3个学生为示例：
+输入：1 Zhangsan 90 90 90 2 Lisi 80 80 80 3 Liufei 96 96 96 4 Liqi 100 80 90 5 Zhanghua 77 78 79
+输出：
+num name score
+1 Zhangsan 90 90 90
+2 Lisi 80 80 80
+3 Liufei 96 96 96
+4 Liqi 100 80 90
+5 Zhanghua 77 78 79*/
